0a7802d7be21eb41e47bf171694317e1b69ea600
Michael Cumming <michael@ambitiouscity.com>
2015-07-09 15:49:00 -0400

    working arrows

--- content/articles/overall/graph-LATEST.rst
+++ content/articles/overall/graph-LATEST.rst
@@ -6,25 +6,13 @@ Graph-LATEST
 :tags: Amenity
 :category: Graph
 
-.. <link rel="stylesheet" type="text/css" href="code/css/graphParts.css">
 
 .. raw:: html
 
 	<!DOCTYPE html>
 	<meta charset="utf-8">
-	<style>
 
-		.node {
-		  stroke: #fff;
-		  stroke-width: 5px;
-		}
-
-		.link {
-		  stroke: #999;
-		  stroke-opacity: .6;
-		}
-
-	</style>
+	<link rel="stylesheet" type="text/css" href="/code/css/graphParts.css">
 
 	<body>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
--- content/code/javascript/graph-LATEST.js
+++ content/code/javascript/graph-LATEST.js
@@ -3,6 +3,7 @@ var width = 500,
 
 var color = d3.scale.category10();
 
+//Set up the force layout
 var force = d3.layout.force()
     .linkDistance(100) //distance we desire between connected nodes; greater the number, nodes farther apart
     // link distance is the expected distance between nodes => http://stackoverflow.com/questions/17355128/relation-between-linkdistance-and-linkstrength-in-d3-js-force-layout
@@ -11,10 +12,12 @@ var force = d3.layout.force()
     //negative charge values indicate repulsion, which is generally desirable for force-directed graphs
     .size([width, height]);
 
+//Append a SVG to the body of the html page. Assign this SVG as an object to var svg
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
+//Read the data from the json data file
 d3.json("/code/json/graph-LATEST.json", function(error, graph) {
     if (error) throw error;
 
@@ -38,41 +41,57 @@ d3.json("/code/json/graph-LATEST.json", function(error, graph) {
         });
     });
 
-    //original:
-    // graph.links.forEach(function(link) {
-    //     var s = nodes[link.source],
-    //         t = nodes[link.target],
-    //         i = {}; // intermediate node
-
-    //     nodes.push(i);
-    //     links.push({
-    //         source: s,
-    //         target: i
-    //     }, {
-    //         source: i,
-    //         target: t
-    //     });
-    //     bilinks.push([s, i, t]);
-    // });
-
+    //Creates the graph data structure out of the json data
     force
         .nodes(nodes)
         .links(links)
         .start();
 
+    //---Insert-------
+    svg.append("defs").selectAll("marker")
+        .data(["suit", "licensing", "resolved"])
+        .enter().append("marker")
+        .attr("id", function(d) {
+            return d;
+        })
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 25)
+        .attr("refY", 0)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+        .append("path")
+        .attr("d", "M0,-5L10,0L0,5 L10,0 L0, -5")
+        .style("stroke", "#999")
+        .style("opacity", "0.85");
+    //---End Insert---
+
+    //Create all the line svgs but without locations yet
     var link = svg.selectAll(".link")
         .data(links)
         .enter().append("line")
         .attr("class", "link")
-        .style("stroke-width", function(d) {
-            return Math.sqrt(d.value);
-        });
+        .style("marker-end", "url(#suit)") // Modified line 
+    ;
+    // .attr("class", function(d) {
+    //     return "link " + d.type;
+    // })
+    // .style("stroke-width", function(d) {
+    //     return Math.sqrt(d.value);
+    // })
 
+    // .attr("marker-end", function(d) {
+    //     return "url(#" + d.type + ")";
+    // })
+
+
+
+    //Do the same with the circles for the nodes
     var node = svg.selectAll(".node")
         .data(nodes)
         .enter().append("circle")
         .attr("class", "node")
-        .attr("r", 15)
+        .attr("r", 12)
         .style("fill", function(d) {
             return color(d.group);
         })
@@ -83,6 +102,9 @@ d3.json("/code/json/graph-LATEST.json", function(error, graph) {
             return d.name;
         });
 
+
+
+    //Give the SVGs co-ordinates - the force layout generates co-ordinates that this code uses to update the attributes of the SVG elements
     force.on("tick", function() {
         link.attr("x1", function(d) {
                 return d.source.x;
@@ -104,4 +126,5 @@ d3.json("/code/json/graph-LATEST.json", function(error, graph) {
                 return d.y;
             });
     });
+
 });
