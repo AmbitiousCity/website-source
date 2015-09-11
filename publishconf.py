980d6e4b83a35cb6168fa331e00c588748d5fa8a
Michael Cumming <michael@ambitiouscity.com>
2015-09-11 16:12:47 -0400

    test

--- publishconf.py
+++ publishconf.py
@@ -10,11 +10,12 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://ambitiouscity.com' #does this work?
-# SITEURL = 'https://ambitiouscity.github.io' #this works for feeds
+# SITEURL = 'http://ambitiouscity.com' #does this work? no; css missing
+SITEURL = 'https://ambitiouscity.github.io' #this works for feeds
+FEED_DOMAIN = 'https://ambitiouscity.github.io'
 #valid feed FYI: http://ambitiouscity.github.io/feeds/all.atom.xml
 #this feed doesn't work (because of redirect): http://ambitiouscity.com/feeds/all.atom.xml
-FEED_DOMAIN = 'https://ambitiouscity.github.io'
+
 GOOGLE_ANALYTICS = 'UA-44332815-1'
 #DISQUS_SITENAME = ""
 
